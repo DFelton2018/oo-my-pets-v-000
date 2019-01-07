0fa1db8fbfe5308442ca18490af15927069edc63
Duane Felton <dfelton2010@gmail.com>
2019-01-07 15:49:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,17 +1,19 @@
 class Owner
   attr_accessor :pets
   
-  
-  @pets = {fishes: [], cats: [], dogs: []}
+  @@all = []
+ 
   
   def initialize
-    
-    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
   end
   
   def self.all
     @@all
   end
+  
+  def self.count
+    
   
   
   
